5cc073d9e432d62b7e3bfd34f8a3b31935713f48
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2023-01-31 09:26:51 +0100

    Make it cmpatible with AIX's "malloc.h"

--- sys/types.h
+++ sys/types.h
@@ -3,4 +3,14 @@
 #ifndef SYS_TYPES_H
 #define SYS_TYPES_H
 
+/* On AIX, 'sys/types.h' defines 'size_t' and 'uintptr_t'
+   (at least 'malloc.h' suggests so), we solve that via includes.
+
+   Note: these files are meant only for the precompiling,
+   not for normal usage.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
 #endif
