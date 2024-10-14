0f1e7194628ab43fa1d13fa4d411452036a794ab
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2024-10-14 11:10:31 +0200

    Original upload

---
+++ alloca.h
@@ -0,0 +1,12 @@
+/* alloca.h */
+
+/* not for actual usage, only to make preprocessors like Pro*C happy /
+
+#ifndef _ALLOCA_H
+#define _ALLOCA_H
+
+#include <stddef.h>
+
+void *alloca (size_t);
+
+#endif
